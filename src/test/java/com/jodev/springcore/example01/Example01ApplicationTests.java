a140a2baab244a48916272944799403001b275d2
Joao Marcos R de Carvalho <jc808853@hpeautos.com.br>
2021-02-20 14:07:06 -0300

    first commit

---
+++ src/test/java/com/jodev/springcore/example01/Example01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jodev.springcore.example01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
